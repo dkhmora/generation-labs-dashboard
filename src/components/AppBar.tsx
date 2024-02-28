7b363d246883b83a1917c916359262032469ece7
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-28 10:57:29 -0800

    Update AppBar component to include profile circle image

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,10 +1,11 @@
 import logo from "../assets/logo.svg";
 import MobileNavigationDrawer from "./MobileNavigationDrawer";
+import ProfileCircleImage from "../assets/profile_circle.svg";
 
 export default function AppBar() {
   return (
     <header>
-      <div className="flex items-center w-full px-6 bg-white shadow-md justify-between md:py-4 md:px-16 lg:px-32">
+      <div className="flex items-center w-full px-6 bg-white shadow-md justify-between md:py-4 md:px-16 md:shadow-none lg:px-32">
         <div className="flex justify-start md:hidden">
           <MobileNavigationDrawer />
         </div>
@@ -14,6 +15,12 @@ export default function AppBar() {
         </div>
 
         <div className="flex justify-end w-8"></div>
+
+        <img
+          src={ProfileCircleImage}
+          alt="logo"
+          className="h-auto w-auto max-h-10"
+        />
       </div>
     </header>
   );
