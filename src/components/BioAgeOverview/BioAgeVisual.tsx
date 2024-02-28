977724d6854e95a0b570fcb94158cde0ebb8656e
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-27 16:55:00 -0800

    Refactor BioAgeVisual component and add padding to BioAgeOverview

--- src/components/BioAgeOverview/BioAgeVisual.tsx
+++ src/components/BioAgeOverview/BioAgeVisual.tsx
@@ -10,16 +10,32 @@ export default function BioAgeVisual(BioAgeVisualProps: BioAgeVisualProps) {
 
   return (
     <div className="bg-[#D94100] rounded-[100px] h-14 grid grid-cols-12 justify-center content-center">
-      <div className="col-start-5 col-span-4">
-        <p className="text-center text-white text-xl font-bold leading-3">
-          {chronoAge}
-        </p>
+      <div className="relative col-start-5 col-span-4">
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col absolute bottom-0 justify-center items-center text-center content-center">
+            <p className="text-[#7D2703] font-medium">ChronoAge</p>
+
+            <div className="w-px h-12 bg-[#D94100]" />
+          </div>
+
+          <p className="text-center text-white text-xl font-bold leading-3">
+            {chronoAge}
+          </p>
+        </div>
       </div>
 
-      <div className="col-span-3 col-span-4">
-        <p className="text-center text-white text-3xl font-bold leading-3">
-          {bioAge}
-        </p>
+      <div className="relative col-span-3 col-span-4">
+        <div className="flex flex-row justify-center">
+          <div className="flex flex-col absolute bottom-0 justify-center items-center text-center content-center">
+            <p className="text-[#7D2703] font-bold">BioAge</p>
+
+            <div className="w-px h-16 bg-[#D94100]" />
+          </div>
+
+          <p className="text-center text-white text-3xl font-bold leading-3">
+            {bioAge}
+          </p>
+        </div>
       </div>
     </div>
   );
--- src/components/BioAgeOverview/index.tsx
+++ src/components/BioAgeOverview/index.tsx
@@ -7,7 +7,9 @@ export default function index() {
       <div className="space-y-4">
         <h2 className="text-2xl font-bold">BioAge Overview</h2>
 
-        <BioAgeVisual chronoAge={40} bioAge={43} />
+        <div className="pt-8">
+          <BioAgeVisual chronoAge={40} bioAge={43} />
+        </div>
       </div>
     </section>
   );
