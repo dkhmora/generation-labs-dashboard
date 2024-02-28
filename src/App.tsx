8c23458f22a3770d2f6c575d3ef7ece4b0e04d24
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-28 10:34:10 -0800

    Added responsive layout for main app

--- src/App.tsx
+++ src/App.tsx
@@ -9,12 +9,16 @@ function App() {
     <main>
       <AppBar />
 
-      <div className="px-4 py-6 space-y-16">
-        <DailyToDos />
+      <div className="flex flex-col px-4 py-6 space-y-16 xl:px-40 lg:flex-row lg:px-20 lg:py-12 lg:space-x-24 lg:space-y-0">
+        <div className="flex flex-1">
+          <DailyToDos />
+        </div>
 
-        <BioAgeOverview />
+        <div className="flex flex-col flex-1 space-y-16">
+          <BioAgeOverview />
 
-        <LogisticTracker />
+          <LogisticTracker />
+        </div>
       </div>
     </main>
   );
