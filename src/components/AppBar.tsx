e9e72d2cd45a9e828f317a8bfd0ca6256268285e
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-28 10:52:09 -0800

    Update AppBar styling

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -4,12 +4,12 @@ import MobileNavigationDrawer from "./MobileNavigationDrawer";
 export default function AppBar() {
   return (
     <header>
-      <div className="flex items-center w-full px-6 bg-white shadow-md justify-between">
-        <div className="flex justify-start">
+      <div className="flex items-center w-full px-6 bg-white shadow-md justify-between md:py-4 md:px-16 lg:px-32">
+        <div className="flex justify-start md:hidden">
           <MobileNavigationDrawer />
         </div>
 
-        <div className="flex justify-center flex-grow">
+        <div className="flex justify-center flex-grow md:justify-start">
           <img src={logo} alt="logo" className="h-auto w-auto max-h-10" />
         </div>
 
