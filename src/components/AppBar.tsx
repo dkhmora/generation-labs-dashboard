c689911184349bc46d4b7d6f6d1c8b1b6cefe8e8
Daryl Kiel Mora <dkhmora@gmail.com>
2024-02-27 18:28:09 -0800

    Refactor AppBar component layout

--- src/components/AppBar.tsx
+++ src/components/AppBar.tsx
@@ -1,18 +1,20 @@
 import React from "react";
 import logo from "../assets/logo.png";
-import { Menu as MenuIcon } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
 import MobileNavigationDrawer from "./MobileNavigationDrawer";
 
 export default function AppBar() {
   return (
     <header>
-      <div className="flex flex-row w-full py-4 px-6 bg-white align-center justify-start content-between space-x-8 shadow-md">
-        <MobileNavigationDrawer />
-
-        <div className="w-full align-center justify-center">
-          <img src={logo} alt="logo" className="h-full self-center" />
+      <div className="flex items-center w-full py-2 px-6 bg-white shadow-md justify-between">
+        <div className="flex justify-start">
+          <MobileNavigationDrawer />
         </div>
+
+        <div className="flex justify-center flex-grow">
+          <img src={logo} alt="logo" className="h-auto w-auto max-h-12" />
+        </div>
+
+        <div className="flex justify-end w-8"></div>
       </div>
     </header>
   );
